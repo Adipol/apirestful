d4a3ca7d8dd2a2551f3be2977ad740f327ae18fd
george <adipol13@gmail.com>
2018-07-22 17:49:12 -0400

    prueba2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hola como estas
+                    nueva version
                 </div>
 
                 <div class="links">
