a61b80586b65956ab3194d353a6356c6d34a366b
george <adipol13@gmail.com>
2018-07-22 17:10:21 -0400

    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hola como estas
                 </div>
 
                 <div class="links">
