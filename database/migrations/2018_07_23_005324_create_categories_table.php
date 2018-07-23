34a0e7e41769cfc7c56089636944e51e9fc2b3ec
george <adipol13@gmail.com>
2018-07-23 12:25:55 -0400

    migracion de categorias

--- database/migrations/2018_07_23_005324_create_categories_table.php
+++ database/migrations/2018_07_23_005324_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Product;
 
 class CreateCategoriesTable extends Migration
 {
@@ -15,6 +16,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('description',1000);
             $table->timestamps();
         });
     }
