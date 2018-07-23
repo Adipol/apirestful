b8e543508b5901ef2cb5c2f1630f12755a0b540d
george <adipol13@gmail.com>
2018-07-23 16:04:15 -0400

    factorias desde databaseseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Category;
+use App\Product;
+use App\Transaction;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +17,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // Dejar nulo las claves foraneas
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate();
+
+        $catidadUsuarios       = 200;
+        $cantidadCategorias    = 30;
+        $cantidadProductos     = 1000;
+        $cantidadTransacciones = 1000;
+
+        factory(User::class,$catidadUsuarios)->create();
+        factory(Category::class,$cantidadCategorias)->create();
+        factory(Product::class,$cantidadProductos)->create()->each(
+            function($producto){
+                $categorias=Category::all()->random(mt_rand(1,5)->pluck('id'));
+                $producto->categories()->attach($categorias);
+            }
+        );
+
+        factory(Transaction::class, $cantidadTransacciones)->create();
     }
 }
