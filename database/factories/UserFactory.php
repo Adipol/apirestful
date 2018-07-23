4d8a51493599de286a304c4567a72955b560355f
george <adipol13@gmail.com>
2018-07-23 14:34:44 -0400

    factory category y product

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 use Faker\Generator as Faker;
 use App\User;
+use App\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,13 +17,31 @@ use App\User;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
-        'verified'=>$vericado=$faker->randomElement([User::USUARIO_VERIFICADO,User::USUARIO_NO_VERIFICADO]),
-        'verification_token'=>$verificado == User::USUARIO_VERIFICADO ? null : User::generarVerificationToken(),
-        'admin'=> $faker->randomElement([User::USUARIO_ADMINISTRADOR,User::USUARIO_REGULAR]),
+        'name'               => $faker->name,
+        'email'              => $faker->unique()->safeEmail,
+        'password'           => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',                            // secret
+        'remember_token'     => str_random(10),
+        'verified'           => $vericado=$faker->randomElement([User::USUARIO_VERIFICADO,User::USUARIO_NO_VERIFICADO]),
+        'verification_token' => $verificado == User::USUARIO_VERIFICADO ? null : User::generarVerificationToken(),
+        'admin'              => $faker->randomElement([User::USUARIO_ADMINISTRADOR,User::USUARIO_REGULAR]),
+    ];
+});
 
+$factory->define(App\Category::class, function (Faker $faker) {
+    return [
+        'name'        => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'name'        => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity'    => $faker->numberBetween(1,10),
+        'status'      => $faker->randomElement([Product::PRODUCTO_DISPONIBLE,Product::PRODUCTO_NO_DISPONIBLE]),
+        'image'       => $faker->randomElement(['1.jpg'.'2.jpg','3.jpg']),
+        //'seller_id'=>User::inRandomOrder()->first()->id,
+        'seller_id' => User::all()->random()->id,
     ];
 });
