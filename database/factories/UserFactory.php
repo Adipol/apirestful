ccc46e822b55c61e902980df74739137254e84f6
george <adipol13@gmail.com>
2018-07-23 15:21:56 -0400

    factory transaction

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 use Faker\Generator as Faker;
 use App\User;
 use App\Product;
+use App\Seller;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,8 +41,20 @@ $factory->define(App\Product::class, function (Faker $faker) {
         'description' => $faker->paragraph(1),
         'quantity'    => $faker->numberBetween(1,10),
         'status'      => $faker->randomElement([Product::PRODUCTO_DISPONIBLE,Product::PRODUCTO_NO_DISPONIBLE]),
-        'image'       => $faker->randomElement(['1.jpg'.'2.jpg','3.jpg']),
+        'image'       => $faker->randomElement(['1.png'.'2.png','3.png']),
         //'seller_id'=>User::inRandomOrder()->first()->id,
         'seller_id' => User::all()->random()->id,
+    ];
+});
+
+$factory->define(App\Transaction::class, function (Faker $faker) {
+
+    $vendedor  = Seller::has('products')->get()->random();
+    $comprador = User::all()->excepr($vendedor->id)->random();
+
+    return [
+        'quantity'   => $faker->numberBetween(1,3),
+        'buyer_id'   => $comprador->id,
+        'product_id' => $vendedor->products->random(),
     ];
 });
