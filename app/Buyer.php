18493dbd8cc91fdd832d6022d31729e95f3d027b
george <adipol13@gmail.com>
2018-07-22 22:38:03 -0400

    relaciones entre los modelos

--- app/Buyer.php
+++ app/Buyer.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App;
+use App\Transaction;
 
 //use Illuminate\Database\Eloquent\Model;
 //recursos=modelos
 class Buyer extends User
 {
-    //
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
--- app/Seller.php
+++ app/Seller.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App;
+use App\Product;
 
 //use Illuminate\Database\Eloquent\Model;
 
 //se extiende de User y no de Model
 class Seller extends User
 {
-    //
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
