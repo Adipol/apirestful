e529057d0f897637765c0a160a0dd6478160de59
george <adipol13@gmail.com>
2018-07-24 15:42:49 -0400

    delete de usercontroller

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -122,6 +122,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['data'=>$user], 200);
     }
 }
