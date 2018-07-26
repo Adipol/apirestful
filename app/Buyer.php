f02459988902aa33f7881ac6d7c502e98f7a898e
george <adipol13@gmail.com>
2018-07-26 16:43:52 -0400

    Resolviendo la inyeccion de buyer usando "global scopes"

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,11 +2,20 @@
 
 namespace App;
 use App\Transaction;
+use App\Scopes\BuyerScope;
 
 //use Illuminate\Database\Eloquent\Model;
 //recursos=modelos
 class Buyer extends User
 {
+    protected static function boot()
+    {
+        //para que laravel inicialize normalmente
+        parent::boot();
+
+        static::addGlobalScope(new BuyerScope);
+    }
+
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
