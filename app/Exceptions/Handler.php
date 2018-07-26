ea9718448b08a5c9eb7bbd143c728424b20c8f9f
george <adipol13@gmail.com>
2018-07-26 15:59:22 -0400

    Manejando excepciones inesperadas

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -97,7 +97,11 @@ class Handler extends ExceptionHandler
             }
         }
 
-        return parent::render($request, $exception);
+        if (config('app.debug')) {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Falla inesperada. Intente luego', 500);
     }
 
     protected function unauthenticated($request, AuthenticationException $exception)
