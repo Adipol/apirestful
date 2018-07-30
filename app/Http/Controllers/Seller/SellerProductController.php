2e69fd27e26e1e8cc0a9c84590e369922b218b74
george <adipol13@gmail.com>
2018-07-30 17:20:22 -0400

    implementando el metodo store para SellerProductController

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Seller;
 use App\Seller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
+use App\Product;
+use App\User;
 
 class SellerProductController extends ApiController
 {
@@ -18,6 +20,26 @@ class SellerProductController extends ApiController
         $products = $seller->products;
 
         return $this->showAll($products);
-        
+    }
+    //se pudo User en vez de Seller por un nuevo usuario que no este como seller
+    public function store(Request $request, User $seller)
+    {
+        $rules =[
+            'name' => 'required',
+            'description' => 'required',
+            'quantity'=>'required|integer|min:1',
+            'imagen'=>'required|image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+        $data['status']= Product::PRODUCTO_NO_DISPONIBLE;
+        $data['image']='i.jpg';
+        $data['seller_id']=$seller->id;
+
+        $product =Product::create($data);
+
+        return $this->showOne($product, 201);
     }
 }
