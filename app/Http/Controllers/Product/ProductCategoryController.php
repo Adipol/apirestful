f6feb481674f64dba0ffff110ad69870375a8fa4
george <adipol13@gmail.com>
2018-08-03 14:43:17 -0400

    implementacion del metodo destroy para productCategoryController

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -32,4 +32,15 @@ class ProductCategoryController extends ApiController
 
         return $this->showAll($product->categories);
     }
+
+    public function destroy(Product $product, Category $category)
+    {
+        if(!$product->categories()->find($category->id)){
+            return $this->errorResponse('La categoria especificada no es una categoria de este producto.', 404);
+        }
+
+        $product->categories()->detach([$category->id]);
+
+        return $this->showAll($product->categories);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::resource('categories.transactions', 'Category\CategoryTransactionControll
  * Products
  */
 Route::resource('products', 'Product\ProductController',['only'=>['index','show']]);
-Route::resource('products.categories', 'Product\ProductCategoryController',['only'=>['index','update']]);
+Route::resource('products.categories', 'Product\ProductCategoryController',['only'=>['index','update','destroy']]);
 Route::resource('products.transactions', 'Product\ProductTransactionController',['only'=>['index']]);
 Route::resource('products.buyers', 'Product\ProductBuyerController',['only'=>['index']]);
 
