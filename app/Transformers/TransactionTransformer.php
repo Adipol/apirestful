afc58b4ad7fc829b0200c05053522a01ce831fcb
george <adipol13@gmail.com>
2018-08-08 15:39:08 -0400

    implementando HATEOAS para instancias de transaction

--- app/Transformers/TransactionTransformer.php
+++ app/Transformers/TransactionTransformer.php
@@ -18,10 +18,33 @@ class TransactionTransformer extends TransformerAbstract
             'identificador'      => (int)$transaction->id,
             'cantidad'           => (string)$transaction->quantity,
             'comprador'          => (string)$transaction->buyer_id,
-            'producto'           => (int)$transaction->product_id,
+            'transactiono'           => (int)$transaction->transaction_id,
             'fechaCreacion'      => (string)$transaction->created_at,
             'fechaActualizacion' => (string)$transaction->updated_at,
             'fechaEliminacion'   => isset($transaction->deleted_at) ? (string) $transaction->deleted_at : null,
+            //HATEOAS
+            'links'=>[            
+                [
+                    'rel' => 'self',
+                    'href'=> route('transactions.show', $transaction->id),
+                ],
+                [
+                    'rel'=>'transaction.categories',
+                    'href'=>route('transactions.categories.index',$transaction->id),
+                ],
+                [
+                    'rel'=>'transaction.seller',
+                    'href'=>route('transactions.sellers.index',$transaction->id),
+                ],
+                [
+                    'rel'=>'buyer',
+                    'href'=>route('buyers.show',$transaction->buyer_id),
+                ],
+                [
+                    'rel'=>'product',
+                    'href'=>route('products.show',$transaction->product_id),
+                ]
+            ],
         ];
     }
 
@@ -31,7 +54,7 @@ class TransactionTransformer extends TransformerAbstract
             'identificador'      => 'id',
             'cantidad'           => 'quantity',
             'comprador'          => 'buyer_id',
-            'producto'           => 'product_id',
+            'transactiono'           => 'transaction_id',
             'fechaCreacion'      => 'created_at',
             'fechaActualizacion' => 'updated_at',
             'fechaEliminacion'   => 'deleted_at',
